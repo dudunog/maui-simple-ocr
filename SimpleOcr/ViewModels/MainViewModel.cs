ed32037be07f6598b76563ed7afd35dfa107e86a
Eduardo Nogueira <eduardodudunog@gmail.com>
2022-12-03 13:42:00 -0300

    feat: add app

---
+++ SimpleOcr/ViewModels/MainViewModel.cs
@@ -0,0 +1,123 @@
+﻿using DotNeuralNet;
+using Newport;
+using Newport.Commands;
+using Newport.Controls;
+using Newport.ViewModels;
+using System.Windows.Input;
+
+namespace SimpleOcr.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private DotNeuralNet.Network _network;
+        private List<BackPropagationTrainingRow> _trainingRows;
+        private int _selectedNumber;
+        private bool _isTraining;
+        private bool _isSnowing;
+
+        public MainViewModel()
+        {
+            InitNeuralNetwork();
+
+            Text = "Simple OCR";
+            Rows = Cols = 10;
+            IsTraining = true;
+
+            Cells = Enumerable.Range(0, Rows * Cols).Select(_ => new CellViewModel()).ToList();
+            Numbers = Enumerable.Range(0, 10).ToList();
+
+            ResetCommmand = new ActionCommand(_ => Reset());
+            OkCommmand = new ActionCommand(_ =>
+            {
+                _network.Invalidate();
+                if (IsTraining)
+                {
+                    var row = BackPropagationTrainingRow.Create(_network);
+                    Cells.ForEach((i, c) => row.Inputs[i] = c.IsChecked ? 1 : 0);
+                    for (var i = 0; i < 10; i++)
+                    {
+                        row.Outputs[i] = (SelectedNumber == i) ? 1.0 : 0.0;
+                    }
+                    _trainingRows.Add(row);
+                }
+                else
+                {
+                    // Feed input data to neural network.
+                    Cells.ForEach((i, c) => _network.InputNodes[i++].Value = c.IsChecked ? 1 : 0);
+                    // Find node with highest activation.
+                    var node = _network.OutputNodes.First(n => n.Value == _network.OutputNodes.Max(nn => nn.Value));
+                    SelectedNumber = _network.OutputNodes.IndexOf(node);
+                }
+                Reset();
+            });
+            InitParticleCommand = new GenericActionCommand<Particle>(p =>
+            {
+                p.Position.X = RandomData.GetDouble(400);
+                p.Position.Y = 0;
+                p.Velocity.Y = Math.Abs(p.Velocity.Y);
+                p.Color = p.Color.WithAlpha((float)RandomData.GetDouble(0.5, 0.9));
+            });
+
+        }
+
+        public ICommand InitParticleCommand { get; }
+
+        private async void InitNeuralNetwork()
+        {
+            _trainingRows = new List<BackPropagationTrainingRow>();
+            _network = new(100, 60, 10);
+            var weights = await IsolatedStorageHelper.Load<double[]>("weights.xml");
+            if (weights != null)
+            {
+                _network.SetWeights(weights);
+            }
+        }
+
+        public int Cols { get; set; }
+
+        public int Rows { get; set; }
+
+        public bool IsSnowing
+        {
+            get => _isSnowing;
+            set => SetProperty(ref _isSnowing, value);
+        }
+
+        public bool IsTraining
+        {
+            get
+            {
+                return _isTraining;
+            }
+            set
+            {
+                SetProperty(ref _isTraining, value);
+            }
+        }
+
+        public IEnumerable<CellViewModel> Cells { get; private set; }
+
+        public ActionCommand ResetCommmand { get; private set; }
+
+        public ActionCommand OkCommmand { get; private set; }
+
+        public List<int> Numbers { get; private set; }
+
+        public int SelectedNumber
+        {
+            get
+            {
+                return _selectedNumber;
+            }
+            set
+            {
+                SetProperty(ref _selectedNumber, value);
+            }
+        }
+
+        private void Reset()
+        {
+            Cells.ForEach(c => c.IsChecked = false);
+        }
+    }
+}
